34444c8598d829625de97bebb5bcd7ec86e8977f
Yacouri <zouhiryac1@gmail.com>
2022-05-22 23:25:43 +0100

    add global config file for svg icons

---
+++ config/global.d.ts
@@ -0,0 +1,10 @@
+export {};
+
+declare global {
+  //  Declare global props for svg icons
+  type SvgIconProps = {
+    color: string;
+    size: number | string;
+    fill?: 'none' | string;
+  };
+}
