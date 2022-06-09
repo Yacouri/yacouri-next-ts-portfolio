8b37c95cece87a3ef873a3146a2a68b74ce530ba
Yacouri <zouhiryac1@gmail.com>
2022-06-09 23:45:43 +0100

    add more config to icons

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: '@svgr/webpack'
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }]
     });
     return config;
   }
