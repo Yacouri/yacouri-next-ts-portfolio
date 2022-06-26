4840c55ec4adbeae646e082d3e01eb890b7a72f1
Yacouri <zouhiryac1@gmail.com>
2022-06-26 13:39:48 +0100

    add site infos object

--- utils/index.ts
+++ utils/index.ts
@@ -17,3 +17,19 @@ export const getArticle = (slug: string) => {
     return data;
   });
 };
+
+export const siteInfos = {
+  media: {
+    twitter: 'https://twitter.com/zyacouri',
+    github: 'https://github.com/yacouri',
+    behance: 'https://behance.net/zouhiryacouri',
+    dribbble: 'https://dribbble.com/yacouri'
+  },
+  title: 'Yacouri - Personal Portfolio',
+  author: 'Zouhir Yacouri',
+  url: 'https://yacouri.com',
+  description:
+    "I'm Zouhir Yaçouri AKA YACOURI, a front end web developer & self-taught Ui/Ux designer as well.",
+  contact: 'co.yacouri@gmail.com',
+  googleAnalyticsId: 'G-81ZSD8WJDC'
+};
