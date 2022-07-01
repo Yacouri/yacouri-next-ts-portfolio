de72a88cf353463d1ac685e63003d5cad5696aef
Yacouri <zouhiryac1@gmail.com>
2022-07-01 22:19:38 +0100

    fix google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider } from 'styled-components';
 import useTheme from '../hooks/useTheme';
 import Layout from '../layouts/Layout';
@@ -7,9 +8,24 @@ import { darkMode, lightMode } from '../styles/themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { theme } = useTheme();
-  
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
   return (
     <ThemeProvider theme={theme === 'light' ? lightMode : darkMode}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="gtg" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
       <Layout>
         <GlobalStyle />
         <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,21 +38,6 @@ class CustomDocument extends Document {
   render() {
     return (
       <Html>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-
-        <Script id="gtg" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-            });
-        `}
-        </Script>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
